dd6eb75b8c5317af442618b459e8f043132c29ff
abhilash <abhilash.kamble376@gmail.com>
2020-12-10 10:52:59 +0530

    server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express= require('express')
+
+
+const app=express()
+
+
+app.listen(8080,'0.0.0.0',(req,res)=>{
+    console.log(`application started`)
+})
+
+app.get('/',(req,res)=>{
+res.send(`hello`)
+})
