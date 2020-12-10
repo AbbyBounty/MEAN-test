73262475c3327915bacd2f60613a749667e0c463
abhilash <abhilash.kamble376@gmail.com>
2020-12-10 12:45:31 +0530

    category array

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,10 +3,17 @@ const express=require('express')
 
 const router=express.Router()
 
-
+const categories=[
+    {id:1,title:'category 1',description:'this is description'},
+    {id:1,title:'category 1',description:'this is description'},
+    {id:1,title:'category 1',description:'this is description'},
+    {id:1,title:'category 1',description:'this is description'},
+    {id:1,title:'category 1',description:'this is description'}
+]
 
 router.get('/category',(req,res)=>{
-    res.end("hello from GET category")
+    
+    res.send(categories)
 })
 
 
