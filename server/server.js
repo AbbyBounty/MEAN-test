b4c70d56904bd08989ff730ef96e885d75389a54
abhilash <abhilash.kamble376@gmail.com>
2020-12-10 11:05:24 +0530

    router

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
-const express= require('express')
+const express = require('express')
 
 
-const app=express()
+const app = express()
 
 
-app.listen(8080,'0.0.0.0',(req,res)=>{
+app.listen(8080, '0.0.0.0', (req, res) => {
     console.log(`application started`)
 })
 
-app.get('/',(req,res)=>{
-res.send(`hello`)
+app.get('/', (req, res) => {
+    res.send(`hello`)
+})
+
+app.post('/{id}', (req, res) => {
+
+
+    res.send(req.query(id))
+
 })
