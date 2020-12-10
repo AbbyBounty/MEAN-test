328256baf59e3b61502a5c7d139d42f4d10921e1
abhilash <abhilash.kamble376@gmail.com>
2020-12-10 11:13:19 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,35 +1,38 @@
-const express = require('express')
+// const express = require('express')
 
 
-const app = express()
+// const app = express()
 
 
-app.listen(8080, '0.0.0.0', (req, res) => {
-    console.log(`application started`)
-})
+// app.listen(8080, '0.0.0.0', (req, res) => {
+//     console.log(`application started`)
+// })
 
-app.get('/', (req, res) => {
-    res.send(`hello`)
-})
+// app.get('/', (req, res) => {
+//     res.send(`hello`)
+// })
 
-app.post('/', (req, res) => {
-
-
-    res.send(req.body())
-
-})
+// app.post('/', (req, res) => {
 
 
-app.put('/', (req, res) => {
+//     res.send(req.body())
+
+// })
 
 
-    res.send(req.body())
-
-})
-
-app.delete('/', (req, res) => {
+// app.put('/', (req, res) => {
 
 
-    res.send(req.body())
+//     res.send(req.body())
 
-})
+// })
+
+// app.delete('/', (req, res) => {
+
+
+//     res.send(req.body())
+
+// })
+
+
+
