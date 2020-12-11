26f55a547d60c1fbe55d5c8cd9bd6013c220a5dc
abhilash <abhilash.kamble376@gmail.com>
2020-12-11 10:58:49 +0530

    register done

--- server1/routes/user.js
+++ server1/routes/user.js
@@ -1,8 +1,40 @@
 const express=require('express')
-
+const mysql=require('mysql2')
 const router=express.Router()
 
+router.post('/register',(req,res)=>{
+    const {firstname,lastname,email,password}=req.body
 
+    const connection=mysql.createConnection({
+
+
+        host:'localhost',
+        user:'root',
+        password:'abhicompmit ',
+        port:3306,
+        database:'mean'
+    })
+
+    const statement=`insert into user(firstname,lastname,email,password) values ('${firstname}','${lastname}','${email}','${password}')`
+    
+    connection.query(statement,(error,result)=>{
+        connection.close()
+
+        if(error){
+            res.send(`error:${error}`)
+        }
+
+        else{
+            res.send('ok')
+
+        }
+    })
+   
+
+  
+
+
+})
 
 
 module.exports=router
--- server1/server.js
+++ server1/server.js
@@ -1,16 +1,19 @@
  const express=require('express')
+ const bodyParser=require('body-parser')
+
 
  const app=express()
-const bodyParser=require('body-parser')
 const userRouter=require('./routes/user')
 const noteRouter=require('./routes/note')
+
+
+app.use(bodyParser.json())
 
 
 app.use('/user',userRouter)
 app.use('/note',noteRouter)
 
 
-app.use(bodyParser.json())
 
  app.listen(8080,'0.0.0.0',()=>{
      console.log('server started')
