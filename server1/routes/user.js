3e918cdcd2d86e183bb4e6db090bfa8ac3e4a45b
abhilash <abhilash.kamble376@gmail.com>
2020-12-11 11:34:54 +0530

    crypto-js

--- server1/routes/user.js
+++ server1/routes/user.js
@@ -1,28 +1,20 @@
 const express=require('express')
-const mysql=require('mysql2')
+const db=require('../db')
 const router=express.Router()
 const crypto=require('crypto-js')
 
 router.post('/register',(req,res)=>{
     const {firstname,lastname,email,password}=req.body
 
-    const connection=mysql.createConnection({
-
-
-        host:'localhost',
-        user:'root',
-        password:'abhicompmit',
-        port:3306,
-        database:'mean'
-    })
+  
 
 
     const pwd=crypto.SHA256(password)
 
     const statement=`insert into user(firstname,lastname,email,password) values ('${firstname}','${lastname}','${email}','${pwd}')`
     
-    connection.query(statement,(error,result)=>{
-        connection.close()
+    db.query(statement,(error,result)=>{
+     
 
         if(error){
             res.send(`error:${error}`)
@@ -33,12 +25,33 @@ router.post('/register',(req,res)=>{
 
         }
     })
-   
-
-  
 
 
 })
+
+router.post('/login',(req,res)=>{
+    const {email,password}=req.body
+
+    const pwd=crypto.SHA256(password)
+const statement=`select * from user where email='${email}' and password='${pwd}'`
+db.query(statement,(error,result)=>{
+     
+
+    if(error){
+        res.send(`error:${error}`)
+    }
+
+    else{
+
+        res.send(result)
+
+    }
+})
+
+})
+
+
+
 
 
 module.exports=router
