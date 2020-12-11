305c5a5e9b041a1c7e21f0ce0deca11ea3640dc7
abhilash <abhilash.kamble376@gmail.com>
2020-12-11 11:01:18 +0530

    crypto

--- server1/routes/user.js
+++ server1/routes/user.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const mysql=require('mysql2')
 const router=express.Router()
+const crypto=require('crypto-js')
 
 router.post('/register',(req,res)=>{
     const {firstname,lastname,email,password}=req.body
@@ -10,12 +11,15 @@ router.post('/register',(req,res)=>{
 
         host:'localhost',
         user:'root',
-        password:'abhicompmit ',
+        password:'abhicompmit',
         port:3306,
         database:'mean'
     })
 
-    const statement=`insert into user(firstname,lastname,email,password) values ('${firstname}','${lastname}','${email}','${password}')`
+
+    const pwd=crypto.SHA256(password)
+
+    const statement=`insert into user(firstname,lastname,email,password) values ('${firstname}','${lastname}','${email}','${pwd}')`
     
     connection.query(statement,(error,result)=>{
         connection.close()
